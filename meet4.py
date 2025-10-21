037a223e79d1dfccf46c96b32c71356ae979f7a6
Mayzihardiputra <eziee496@gmail.com>
2025-10-21 13:43:30 +0700

    def kelilling

--- meet4.py
+++ meet4.py
@@ -2,3 +2,6 @@ class PersegiPanjang:
     def __init__(self, panjang, Lebar):
         self.panjang = panjang
         self.lebar = Lebar
+
+    def keliling(self):
+        return 2 * (self.panjang + self.lebar)
